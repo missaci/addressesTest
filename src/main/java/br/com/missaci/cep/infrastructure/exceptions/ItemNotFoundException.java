e1a29e0641ab4b794b67e8f1171b7a5914ba9f93
missaci <mateus.missaci@gmail.com>
2015-09-14 03:39:22 -0300

    Minor corretions

--- src/main/java/br/com/missaci/cep/infrastructure/exceptions/ItemNotFoundException.java
+++ src/main/java/br/com/missaci/cep/infrastructure/exceptions/ItemNotFoundException.java
@@ -10,7 +10,7 @@ package br.com.missaci.cep.infrastructure.exceptions;
  */
 public class ItemNotFoundException extends RuntimeException{
 
-	private static final long serialVersionUID = 4655364871734168517L;
+	private static final long serialVersionUID = 4655364871734168518L;
 
 	public ItemNotFoundException(String message) {
 		super(message);
